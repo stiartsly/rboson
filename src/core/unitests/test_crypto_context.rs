c17ef8a1869634d8392bbf2aeb0bf9cd4f50da6e
stiartsly <stiartsly@gmail.com>
2025-04-03 11:31:48 +0800

    WIP: added testcases for CryptoContext

--- src/core/unitests/test_crypto_context.rs
+++ src/core/unitests/test_crypto_context.rs
@@ -1,6 +1,6 @@
 use crate::core::{
     id::Id,
-    cryptobox,
+    cryptobox::{self, CryptoBox, Nonce},
     signature,
     crypto_context::CryptoContext,
 };
@@ -48,6 +48,41 @@ fn test_encrypt2() {
     let id1 = Id::from(sig_kp1.to_public_key());
     let id2 = Id::from(sig_kp2.to_public_key());
 
+    let ctx1 = CryptoContext::new(&id2, &box_kp1.private_key());
+    let mut ctx2 = CryptoContext::new(&id1, &box_kp2.private_key());
+
+    assert_eq!(&id2, ctx1.id());
+    assert_eq!(&id1, ctx2.id());
+
+    let plain = "Hello, World!".as_bytes();
+    let mut cipher = vec![0u8; plain.len() + CryptoBox::MAC_BYTES + Nonce::BYTES];
+    let result = ctx2.encrypt(plain, &mut cipher);
+    assert!(result.is_ok());
+    assert_eq!(result.unwrap(), cipher.len());
+
+    let result = ctx1.decrypt_into(&cipher);
+    assert!(result.is_ok());
+
+    let decrypted = result.unwrap();
+    assert_eq!(plain, decrypted.as_slice());
+
+    let mut decrypted = vec![0u8; cipher.len() - CryptoBox::MAC_BYTES - Nonce::BYTES];
+    let result = ctx1.decrypt(&cipher, &mut decrypted);
+    assert!(result.is_ok());
+    assert_eq!(result.unwrap(), decrypted.len());
+    assert_eq!(plain, decrypted.as_slice());
+}
+
+#[test]
+fn test_encrypt3() {
+    let sig_kp1 = signature::KeyPair::random();
+    let sig_kp2 = signature::KeyPair::random();
+    let box_kp1 = cryptobox::KeyPair::from(&sig_kp1);
+    let box_kp2 = cryptobox::KeyPair::from(&sig_kp2);
+
+    let id1 = Id::from(sig_kp1.to_public_key());
+    let id2 = Id::from(sig_kp2.to_public_key());
+
     let box1 = cryptobox::CryptoBox::try_from((box_kp2.public_key(), box_kp1.private_key())).unwrap();
     let box2 = cryptobox::CryptoBox::try_from((box_kp1.public_key(), box_kp2.private_key())).unwrap();
     let ctx1 = CryptoContext::from_cryptobox(&id2, box1);
@@ -65,5 +100,42 @@ fn test_encrypt2() {
     assert!(result.is_ok());
 
     let decrypted = result.unwrap();
+    assert_eq!(plain, decrypted.as_slice());
+}
+
+#[test]
+fn test_encrypt4() {
+    let sig_kp1 = signature::KeyPair::random();
+    let sig_kp2 = signature::KeyPair::random();
+    let box_kp1 = cryptobox::KeyPair::from(&sig_kp1);
+    let box_kp2 = cryptobox::KeyPair::from(&sig_kp2);
+
+    let id1 = Id::from(sig_kp1.to_public_key());
+    let id2 = Id::from(sig_kp2.to_public_key());
+
+    let box1 = cryptobox::CryptoBox::try_from((box_kp2.public_key(), box_kp1.private_key())).unwrap();
+    let box2 = cryptobox::CryptoBox::try_from((box_kp1.public_key(), box_kp2.private_key())).unwrap();
+    let ctx1 = CryptoContext::from_cryptobox(&id2, box1);
+    let mut ctx2 = CryptoContext::from_cryptobox(&id1, box2);
+
+    assert_eq!(&id2, ctx1.id());
+    assert_eq!(&id1, ctx2.id());
+
+    let plain = "Hello, World!".as_bytes();
+    let mut cipher = vec![0u8; plain.len() + CryptoBox::MAC_BYTES + Nonce::BYTES];
+    let result = ctx2.encrypt(plain, &mut cipher);
+    assert!(result.is_ok());
+    assert_eq!(result.unwrap(), cipher.len());
+
+    let result = ctx1.decrypt_into(&cipher);
+    assert!(result.is_ok());
+
+    let decrypted = result.unwrap();
+    assert_eq!(plain, decrypted.as_slice());
+
+    let mut decrypted = vec![0u8; cipher.len() - CryptoBox::MAC_BYTES - Nonce::BYTES];
+    let result = ctx1.decrypt(&cipher, &mut decrypted);
+    assert!(result.is_ok());
+    assert_eq!(result.unwrap(), decrypted.len());
     assert_eq!(plain, decrypted.as_slice());
 }

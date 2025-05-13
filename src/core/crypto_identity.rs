602a8dd9d33c3cedc605b4e5786be7d3a914b45e
stiartsly <stiartsly@gmail.com>
2025-05-13 14:37:57 +0800

    Added public methods to get signature keypair and encryption keypair

--- src/core/crypto_identity.rs
+++ src/core/crypto_identity.rs
@@ -1,16 +1,16 @@
 use crate::{
     Id,
-    cryptobox::{self, Nonce},
+    cryptobox::{self, Nonce, CryptoBox},
     signature,
+    error::Result,
     Identity,
-    core::crypto_context::CryptoContext,
-    error::Result
+    core::crypto_context::CryptoContext
 };
 
 #[derive(Clone, Debug)]
 pub struct CryptoIdentity {
     id: Id,
-    keypair: cryptobox::KeyPair,
+    encrypt_keypair: cryptobox::KeyPair,
     signature_keypair: signature::KeyPair,
 }
 
@@ -28,9 +28,23 @@ impl CryptoIdentity {
     pub(crate) fn from_keypair(keypair: signature::KeyPair) -> CryptoIdentity {
         Self {
             id: Id::from(keypair.to_public_key()),
-            keypair: cryptobox::KeyPair::from(&keypair),
+            encrypt_keypair: cryptobox::KeyPair::from(&keypair),
             signature_keypair: keypair,
         }
+    }
+
+    pub fn keypair(&self) -> &signature::KeyPair {
+        &self.signature_keypair
+    }
+
+    pub fn encryption_keypair(&self) -> &cryptobox::KeyPair {
+        &self.encrypt_keypair
+    }
+}
+
+impl PartialEq for CryptoIdentity {
+    fn eq(&self, other: &Self) -> bool {
+        self.id == other.id
     }
 }
 
@@ -45,7 +59,8 @@ impl Identity for CryptoIdentity {
 
     fn sign_into(&self, data: &[u8]) -> Result<Vec<u8>> {
         let mut signature = vec![0u8; signature::Signature::BYTES];
-        self.sign(data, &mut signature).map(|_| signature)
+        self.sign(data, &mut signature)?;
+        Ok(signature)
     }
 
     fn verify(&self, data: &[u8], signature: &[u8]) -> Result<()> {
@@ -58,7 +73,7 @@ impl Identity for CryptoIdentity {
             cipher,
             &Nonce::random(),
             &recipient.to_encryption_key(),
-            self.keypair.private_key()
+            self.encrypt_keypair.private_key()
         )
     }
 
@@ -67,26 +82,25 @@ impl Identity for CryptoIdentity {
             cipher,
             plain,
             &sender.to_encryption_key(),
-            self.keypair.private_key()
+            self.encrypt_keypair.private_key()
         )
     }
 
     fn encrypt_into(&self, recipient: &Id, plain: &[u8]) -> Result<Vec<u8>> {
-        let mut cipher = vec![0u8; plain.len() + cryptobox::CryptoBox::MAC_BYTES + Nonce::BYTES];
-        self.encrypt(recipient, plain, &mut cipher).map(|_| cipher)
+        let mut cipher = vec![0u8; plain.len() + CryptoBox::MAC_BYTES + Nonce::BYTES];
+        self.encrypt(recipient, plain, &mut cipher)?;
+        Ok(cipher)
     }
 
     fn decrypt_into(&self, sender: &Id, data: &[u8]) -> Result<Vec<u8>> {
-        let mut plain = vec![0u8; data.len() - cryptobox::CryptoBox::MAC_BYTES - Nonce::BYTES];
-        self.decrypt(sender, data, &mut plain).map(|_| plain)
+        let mut plain = vec![0u8; data.len() - CryptoBox::MAC_BYTES - Nonce::BYTES];
+        self.decrypt(sender, data, &mut plain)?;
+        Ok(plain)
     }
 
     fn create_crypto_context(&self, id: &Id) -> Result<CryptoContext> {
-        cryptobox::CryptoBox::try_from((
-            &id.to_encryption_key(),
-            self.keypair.private_key()
-        )).map(|box_| {
-            CryptoContext::from_cryptobox(id, box_)
-        })
+        CryptoBox::try_from((&id.to_encryption_key(), self.encrypt_keypair.private_key())).map(|v|
+            CryptoContext::from_cryptobox(id, v)
+        )
     }
 }
